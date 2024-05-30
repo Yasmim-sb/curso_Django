33bc4299bdfe6f4209407d1dd5ec09c13e8e24e5
Yasmim <yasmimsabrinacp@gmail.com>
2024-05-30 20:54:23 -0300

    feat: fazendo ligação através dos módulos e configurando o painel admin do Django

--- learning_log/learning_logs/models.py
+++ learning_log/learning_logs/models.py
@@ -9,3 +9,14 @@ class Topic(models.Model):
         """"Devolve uma representação em string don modelo"""
         return self.text
 
+class Entry(models.Model):
+    """"Algo específico aprendido sobre o assunto"""
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        """"Devolve uma string para o modelo"""
+        return self.text[:50] + '...'
