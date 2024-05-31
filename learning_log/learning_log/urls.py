6bec8746b8baa93d83ee8c803e03f081bc884fea
Yasmim <yasmimsabrinacp@gmail.com>
2024-05-31 15:46:48 -0300

    hello world

--- learning_log/learning_log/urls.py
+++ learning_log/learning_log/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from learning_logs.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home),
 ]
